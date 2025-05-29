647206a01b1933b6179d199c78904bdad1d79172
Moukhtar <99501355+m0ukhtar@users.noreply.github.com>
2025-05-29 19:27:09 +0200

    Update motion_node.py

--- motion_node.py
+++ motion_node.py
@@ -3,30 +3,28 @@ from rclpy.node import Node
 
 from geometry_msgs.msg import Pose2D, Twist, PoseStamped
 from nav_msgs.msg import Path
-
-from tf2_ros import TransformException
-from tf2_ros.buffer import Buffer
-from tf2_ros.transform_listener import TransformListener
+from tf2_ros import Buffer, TransformListener
+from tf2_ros.transform_exception import TransformException
 from tf_transformations import euler_from_quaternion
-
 import math
 
 class Motion(Node):
     def __init__(self):
         Node.__init__(self, "motion_node")
-
         self.tf_buffer = Buffer()
         self.tf_listener = TransformListener(self.tf_buffer, self)
 
-        self.robot_pose = Pose2D()
-        self.goal_received, self.reached = False, False
+        self.robot_pose = Pose2D()  # Current pose of the robot: self.robot_pose.x, self.robot_pose.y, self.robot_pose.theta
+        self.goal_received, self.reached = False, False  # new goal received; whether the goal has been reached
 
-        self.create_subscription(Path, "/path", self.plannerCb, 1)
-        self.robot_path_pub = self.create_publisher(Path, "/robot_path", qos_profile=1)
-        self.vel_pub = self.create_publisher(Twist, "/cmd_vel", 1)
-        self.create_timer(0.1, self.run)
+        self.create_subscription(Path, "/path", self.plannerCb, 1)  # Subscriber to get the path computed by the BiRRT node
+        self.robot_path_pub = self.create_publisher(Path, "/robot_path", qos_profile=1)  # Publisher to send real robot's path to RVIZ
+        self.vel_pub = self.create_publisher(Twist, "/cmd_vel", 1)  # Publisher to send velocity messages to Gazebo
+        self.create_timer(0.1, self.run)  # run the navigation strategy @ 10Hz (DO NOT CHANGE the timer frequency)
 
     def get_robot_pose(self):
+        """ Get the current position of the robot (continuous) in the map reference frame """
+        # DO NOT TOUCH
         try:
             trans = self.tf_buffer.lookup_transform(
                 "map",
@@ -41,16 +39,20 @@ class Motion(Node):
             self.get_logger().info(f"Could not transform base_footprint to map: {e}")
 
     def plannerCb(self, msg):
+        """ Callback method to get incoming path computed by the BiRRT node """
+        # DO NOT TOUCH
         self.reached, self.goal_received = False, True
-        self.path = msg.poses[1:]
-        self.inc = 0
+        self.path = msg.poses[1:]  # remove the robot's pose
+        self.inc = 0  # current index of the waypoint to reach from the path
 
+        # Real path followed by the robot
         self.real_path_msg = Path()
         self.real_path_msg.header.frame_id = "map"
         self.real_path_msg.header.stamp = self.get_clock().now().to_msg()
         self.real_path = []
 
     def run(self):
+        """ Main method called periodically to reach the waypoints """
         if not self.reached and self.goal_received:
             self.get_robot_pose()
             if self.inc >= len(self.path):
@@ -64,8 +66,8 @@ class Motion(Node):
 
             dx = current_goal.x - self.robot_pose.x
             dy = current_goal.y - self.robot_pose.y
-            rho = math.sqrt(dx**2 + dy**2)
 
+            rho = math.sqrt(dx ** 2 + dy ** 2)
             angle_to_goal = math.atan2(dy, dx)
             alpha = self.normalize_angle(angle_to_goal - self.robot_pose.theta)
 
@@ -77,9 +79,11 @@ class Motion(Node):
             self.angular = 2.5 * alpha
 
             self.send_velocities()
-            self.publish_path()
+            self.publish_path()  # this is the last instruction in this if statement!!
 
     def send_velocities(self):
+        """ Send computed linear and angular velocities """
+        # DO NOT TOUCH
         self.linear = self.constrain(self.linear, min=-2.0, max=2.0)
         self.angular = self.constrain(self.angular, min=-3.0, max=3.0)
 
@@ -89,11 +93,23 @@ class Motion(Node):
         self.vel_pub.publish(cmd_vel)
 
     def constrain(self, val, min=-2.0, max=2.0):
+        """ Make sure a value is within the range [min, max] """
+        # DO NOT TOUCH
         if val < min:
             return min
         elif val > max:
             return max
         return val
+
+    def publish_path(self):
+        """ Publish the real path followed by the robot """
+        # DO NOT TOUCH
+        pose = PoseStamped()
+        pose.pose.position.x = self.robot_pose.x
+        pose.pose.position.y = self.robot_pose.y
+        self.real_path.append(pose)
+        self.real_path_msg.poses = self.real_path
+        self.robot_path_pub.publish(self.real_path_msg)
 
     def normalize_angle(self, angle):
         while angle > math.pi:
@@ -102,16 +118,10 @@ class Motion(Node):
             angle += 2.0 * math.pi
         return angle
 
-    def publish_path(self):
-        pose = PoseStamped()
-        pose.pose.position.x = self.robot_pose.x
-        pose.pose.position.y = self.robot_pose.y
-        self.real_path.append(pose)
-        self.real_path_msg.poses = self.real_path
-        self.robot_path_pub.publish(self.real_path_msg)
 
 def main():
     rclpy.init()
+
     node = Motion()
     try:
         rclpy.spin(node)
